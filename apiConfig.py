a261d45a166998e55cc6a7df98d9f007a39d48c3
IvanAmador <50033619+IvanAmador@users.noreply.github.com>
2019-09-27 22:18:42 -0300

    Update apiConfig.py

--- apiConfig.py
+++ apiConfig.py
@@ -1,4 +1,4 @@
-access_token = "150633290-Q0OB8IrIC6pmdaVfupQtG6ezgHRh9DG8AFsfM2un"
-access_token_secret = "wOf1t3muuEiXzvL9HHiqY9QJTY4pA1kvPD9cCP6c5lQgu"
-consumer_key = "WkCt3cQoAXlE3fy7rxJxGruLF"
-consumer_secret = "dKaqbt3CJwi5xANUnCT5J03yS0LB0f5ffYuZqr1Wv16IBppNU7"
+access_token = "access_token"
+access_token_secret = "access_token_secret"
+consumer_key = "consumer_key"
+consumer_secret = "consumer_secret"
